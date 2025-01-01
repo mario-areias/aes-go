0140fe6619ca6ee6733b9fa2fd4aedb9a8b8c7a3
Mario Areias <mario.s.s.areias@gmail.com>
2025-01-01 14:55:44 +1100

    End to end test

---
+++ end_to_end_test.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"testing"
+
+	aesgo "github.com/mario-areias/aes-go/aes-go"
+	"github.com/mario-areias/aes-go/key"
+)
+
+func TestCBCStd(t *testing.T) {
+	k := key.Bit128()
+	iv := key.Bit128().GetBytes()
+
+	aes := aesgo.New(k)
+
+	plaintext := []byte("Let's test if this is working!")
+
+	// encrypt with our implementation and decrypt with std
+	cipher := aes.EncryptCBC(plaintext, iv)
+	decrypted, err := stdDecrypt(cipher, k.GetBytes(), iv)
+	if err != nil {
+		t.Errorf("Error decrypting: %s", err)
+	}
+
+	if plaintextStr := string(plaintext); plaintextStr != string(decrypted) {
+		t.Errorf("Decrypted text does not match plaintext. Got: %s, Expected: %s", decrypted, plaintextStr)
+	}
+
+	// encrypt with std and decrypt with our implementation
+	encrypted, err := stdEncrypt(plaintext, k.GetBytes(), iv)
+	if err != nil {
+		t.Errorf("Error encrypting: %s", err)
+	}
+	decrypted, err = aes.DecryptCBC(encrypted, iv)
+	if err != nil {
+		t.Errorf("Error decrypting: %s", err)
+	}
+
+	if plaintextStr := string(plaintext); plaintextStr != string(decrypted) {
+		t.Errorf("Decrypted text does not match plaintext. Got: %s, Expected: %s", decrypted, plaintextStr)
+	}
+}
+
+func TestPaddingOracleAttackWithStdEncryption(t *testing.T) {
+	k := key.Bit128()
+	iv := key.Bit128().GetBytes()
+
+	plaintext := []byte("Let's test if this attack works!!")
+
+	o := Oracle{key: k, iv: iv}
+
+	stdEncrypted, err := stdEncrypt(plaintext, k.GetBytes(), iv)
+	if err != nil {
+		t.Errorf("Error encrypting: %s", err)
+	}
+
+	decrypted := PaddingOracle(o, stdEncrypted)
+
+	unpadded, err := aesgo.RemovePadding(decrypted)
+	if err != nil {
+		t.Errorf("Error removing padding: %s", err)
+	}
+
+	if plaintextStr := string(plaintext); plaintextStr != string(unpadded) {
+		t.Errorf("Decrypted text does not match plaintext. Got: %s, Expected: %s", unpadded, plaintextStr)
+	}
+}
+
+// Function to stdEncrypt plaintext using AES in CBC mode
+func stdEncrypt(plainText, key, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Pad the plaintext to be a multiple of the block size
+	plainText = pad(plainText, aes.BlockSize)
+
+	// Create a new CBC encrypter
+	mode := cipher.NewCBCEncrypter(block, iv)
+
+	// Encrypt the plaintext
+	cipherText := make([]byte, len(plainText))
+	mode.CryptBlocks(cipherText, plainText)
+
+	// Prepend the IV to the ciphertext for use in decryption
+	return cipherText, nil
+}
+
+// Function to stdDecrypt ciphertext using AES in CBC mode
+func stdDecrypt(cipherText, key, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the IV from the beginning of the ciphertext
+	if len(cipherText) < aes.BlockSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+	// Create a new CBC decrypter
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	// Decrypt the ciphertext
+	plainText := make([]byte, len(cipherText))
+	mode.CryptBlocks(plainText, cipherText)
+
+	// Unpad the plaintext
+	return unpad(plainText)
+}
+
+func pad(src []byte, blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+func unpad(src []byte) ([]byte, error) {
+	length := len(src)
+	unpadding := int(src[length-1])
+	if unpadding > length {
+		return nil, fmt.Errorf("invalid padding")
+	}
+	return src[:(length - unpadding)], nil
+}
